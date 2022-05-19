11a150ecddb53bd1e32992202d05125634d0fff5
jongha1 <whdgk3700@naver.com>
2022-05-19 22:57:28 +0900

    메인페이지 1_buttons 임시저장

--- app/src/main/java/com/example/teamproject/MainPage1_buttons.java
+++ app/src/main/java/com/example/teamproject/MainPage1_buttons.java
@@ -3,6 +3,7 @@ package com.example.teamproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,9 +13,9 @@ import java.util.Random;
 
 public class MainPage1_buttons extends Activity {
 
-    Button btn_sub1;
-    Button btn_sub2;
-    Button btn_sub3;
+    Button btn_rd1;
+    Button btn_rd2;
+    Button btn_rd3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,79 +23,99 @@ public class MainPage1_buttons extends Activity {
         setContentView(R.layout.mainpage1_buttons);
         Random rs = new Random();
 
-        btn_sub1 = findViewById(R.id.btn1);
-        btn_sub2 = findViewById(R.id.btn2);
-        btn_sub3 = findViewById(R.id.btn3);
+        btn_rd1 = findViewById(R.id.btn1);
+        btn_rd2 = findViewById(R.id.btn2);
+        btn_rd3 = findViewById(R.id.btn3);
 
-        List<String> story = new ArrayList<>();
+        String[] randomBtn1 = getResources().getStringArray(R.array.mp1_rdbtn1);
+        Random random1 = new Random();
+        int n1 = random1.nextInt(randomBtn1.length - 0);
+        btn_rd1.setText(randomBtn1[n1]);
 
-        story.add(getResources().getString(R.string.mp1_btn1)); //쇠창살
-        story.add(getResources().getString(R.string.mp1_btn2)); //주머니
-        story.add(getResources().getString(R.string.mp1_btn3)); //무리
+        String[] randomBtn2 = getResources().getStringArray(R.array.mp1_rdbtn2);
+        Random random2 = new Random();
+        int n2 = random2.nextInt(randomBtn2.length - 0);
+        btn_rd2.setText(randomBtn2[n2]);
+
+        String[] randomBtn3 = getResources().getStringArray(R.array.mp1_rdbtn3);
+        Random random3 = new Random();
+        int n3 = random3.nextInt(randomBtn3.length - 0);
+        btn_rd3.setText(randomBtn3[n3]);
+        
+        
+       //List<String> story = new ArrayList<>();
+        String[] Str1 = new String[3];
+        Str1 = getResources().getStringArray(R.array.mp1_rdbtn1);
+        Str1 = getResources().getStringArray(R.array.mp1_rdbtn2);
+        Str1 = getResources().getStringArray(R.array.mp1_rdbtn3);
+        //Log.d("hieorek",Str1[0]);
+        //story.add(getResources().getString(R.string.mp1_btn1)); //쇠창살
+        //story.add(getResources().getString(R.string.mp1_btn2)); //주머니
+        //story.add(getResources().getString(R.string.mp1_btn3)); //무리
 
         int r = rs.nextInt(3);
         int r1 = rs.nextInt(2);
         if (r == 0 && r1 == 0) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn1);
+            btn_rd1.setText(Str1.toString());
+            btn_rd1 = findViewById(R.id.btn1);
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn2);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn2);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn3);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn3);
         } else if (r == 0 && r1 == 1) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn1);
+            btn_rd1.setText(story.get(r));
+            btn_rd1 = findViewById(R.id.btn1);
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn3);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn3);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn2);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn2);
 
         } else if (r == 1 && r1 == 0) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn2);
+            btn_rd1.setText(story.get(r));
+            btn_rd1 = findViewById(R.id.btn2);
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn1);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn1);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn3);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn3);
         } else if (r == 2 && r1 == 0) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn2);
+            btn_rd1.setText(story.get(r));
+            btn_rd1 = findViewById(R.id.btn2);
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn3);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn3);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn1);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn1);
 
         } else if (r == 1 && r1 == 1) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn3);//무리 쇠창살 주머니 : 주머니 무리 쇠창살
+            btn_rd1.setText(story.get(r));
+            btn_rd1 = findViewById(R.id.btn3);//무리 쇠창살 주머니 : 주머니 무리 쇠창살
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn1);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn1);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn2);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn2);
 
         } else if (r == 2 && r1 == 1) {
-            btn_sub1.setText(story.get(r));
-            btn_sub1 = findViewById(R.id.btn3);
+            btn_rd1.setText(story.get(r));
+            btn_rd1 = findViewById(R.id.btn3);
             story.remove(r);
-            btn_sub2.setText(story.get(r1));
-            btn_sub2 = findViewById(R.id.btn2);
+            btn_rd2.setText(story.get(r1));
+            btn_rd2 = findViewById(R.id.btn2);
             story.remove(r1);
-            btn_sub3.setText(story.get(0));
-            btn_sub3 = findViewById(R.id.btn1);
+            btn_rd3.setText(story.get(0));
+            btn_rd3 = findViewById(R.id.btn1);
         }
 
 
-            btn_sub1.setOnClickListener(new View.OnClickListener() {
+            btn_rd1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(getApplicationContext(), MainPage1_button1.class);
@@ -102,7 +123,7 @@ public class MainPage1_buttons extends Activity {
                 }
             });
 
-            btn_sub2.setOnClickListener(new View.OnClickListener() {
+            btn_rd2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(getApplicationContext(), MainPage1_button2.class);
@@ -110,7 +131,7 @@ public class MainPage1_buttons extends Activity {
                 }
             });
 
-            btn_sub3.setOnClickListener(new View.OnClickListener() {
+            btn_rd3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(getApplicationContext(), MainPage1_button3.class);
