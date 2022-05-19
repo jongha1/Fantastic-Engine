8406b8880a87e248c524271c0b774728e848aff3
jongha1 <whdgk3700@naver.com>
2022-05-19 20:16:10 +0900

    메인페이지 11_buttons 랜덤보기 구현(미완성)

--- app/src/main/java/com/example/teamproject/MainPage11_buttons.java
+++ app/src/main/java/com/example/teamproject/MainPage11_buttons.java
@@ -8,25 +8,32 @@ import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainPage11_buttons extends Activity {
-
-    Button btn_sub1;
+    //Button btn_sub1;
     Button btn_sub2;
     Button btn_sub3;
+    Button btn_rd1;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage011_buttons);
 
-        btn_sub1 = findViewById(R.id.btn1);
+      //  btn_sub1 = findViewById(R.id.btn1);
         btn_sub2 = findViewById(R.id.btn2);
         btn_sub3 = findViewById(R.id.btn3);
+        btn_rd1 = findViewById(R.id.rdbtn1);
 
 
-        btn_sub1.setOnClickListener(new View.OnClickListener() {
+        String[] randomBtn = getResources().getStringArray(R.array.mp11_rdbtn1);
+        Random random = new Random();
+        int n = random.nextInt(randomBtn.length - 0);
+        btn_rd1.setText(randomBtn[n]);
+
+        btn_rd1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainPage11_button1.class);
                 startActivity(intent);
             }
