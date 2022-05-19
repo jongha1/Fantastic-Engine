ce2866981dbc8daacd39d3bf0a67e9ff6554ec31
tjrwns1202 <92131363+tjrwns1202@users.noreply.github.com>
2022-05-19 23:30:02 +0900

    랜덤함수 끝.

--- app/src/main/java/com/example/teamproject/MainPage1_buttons.java
+++ app/src/main/java/com/example/teamproject/MainPage1_buttons.java
@@ -41,103 +41,99 @@ public class MainPage1_buttons extends Activity {
         Random random3 = new Random();
         int n3 = random3.nextInt(randomBtn3.length - 0);
         btn_rd3.setText(randomBtn3[n3]);
-        
-        
-       //List<String> story = new ArrayList<>();
-        String[] Str1 = new String[3];
-        Str1 = getResources().getStringArray(R.array.mp1_rdbtn1);
-        Str1 = getResources().getStringArray(R.array.mp1_rdbtn2);
-        Str1 = getResources().getStringArray(R.array.mp1_rdbtn3);
-        //Log.d("hieorek",Str1[0]);
-        //story.add(getResources().getString(R.string.mp1_btn1)); //쇠창살
-        //story.add(getResources().getString(R.string.mp1_btn2)); //주머니
-        //story.add(getResources().getString(R.string.mp1_btn3)); //무리
+
+
+        ArrayList<String> strings = new ArrayList<String>();
+
+        strings.add(btn_rd1.getText().toString());
+        strings.add(btn_rd2.getText().toString());
+        strings.add(btn_rd3.getText().toString());
+
 
         int r = rs.nextInt(3);
         int r1 = rs.nextInt(2);
         if (r == 0 && r1 == 0) {
-            btn_rd1.setText(Str1.toString());
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn1);
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn2);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn3);
         } else if (r == 0 && r1 == 1) {
-            btn_rd1.setText(story.get(r));
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn1);
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn3);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn2);
 
         } else if (r == 1 && r1 == 0) {
-            btn_rd1.setText(story.get(r));
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn2);
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn1);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn3);
         } else if (r == 2 && r1 == 0) {
-            btn_rd1.setText(story.get(r));
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn2);
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn3);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn1);
 
         } else if (r == 1 && r1 == 1) {
-            btn_rd1.setText(story.get(r));
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn3);//무리 쇠창살 주머니 : 주머니 무리 쇠창살
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn1);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn2);
 
         } else if (r == 2 && r1 == 1) {
-            btn_rd1.setText(story.get(r));
+            btn_rd1.setText(strings.get(r));
             btn_rd1 = findViewById(R.id.btn3);
-            story.remove(r);
-            btn_rd2.setText(story.get(r1));
+            strings.remove(r);
+            btn_rd2.setText(strings.get(r1));
             btn_rd2 = findViewById(R.id.btn2);
-            story.remove(r1);
-            btn_rd3.setText(story.get(0));
+            strings.remove(r1);
+            btn_rd3.setText(strings.get(0));
             btn_rd3 = findViewById(R.id.btn1);
         }
 
+        btn_rd1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainPage1_button1.class);
+                startActivity(intent);
+            }
+        });
 
-            btn_rd1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), MainPage1_button1.class);
-                    startActivity(intent);
-                }
-            });
+        btn_rd2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainPage1_button2.class);
+                startActivity(intent);
+            }
+        });
 
-            btn_rd2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getApplicationContext(), MainPage1_button2.class);
-                    startActivity(intent);
-                }
-            });
+        btn_rd3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainPage1_button3.class);
+                startActivity(intent);
+            }
+        });
 
-            btn_rd3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getApplicationContext(), MainPage1_button3.class);
-                    startActivity(intent);
-                }
-            });
-
-        }
     }
+}
