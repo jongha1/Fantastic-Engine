4dcfcdf5a17b72a51b1ec9f0ee59b7339beeb13a
jongha1 <whdgk3700@naver.com>
2022-05-31 00:22:38 +0900

    메인페이지 7~12 이름 기능 구현 (1~6 안된부분만 수정)

--- app/src/main/java/com/example/teamproject/MainPage1.java
+++ app/src/main/java/com/example/teamproject/MainPage1.java
@@ -31,7 +31,7 @@ public class MainPage1 extends Activity {
         tv_mainpage1 = findViewById(R.id.tv_mainpage1);
         tv_mainpage1.setText("오래된 중앙아프리카 공화국 내전을 취재하러 온 "+name+ "은 시민들을 취재하다 혼자 떨어져 폭발물에 휩쓸렸다. " +
                 "눈을 떠 보니, 좁고 고름 냄새로 퀴퀴한 감옥 안이었다. 혹시나 하는 마음에 몸을 움직여 봤지만, " +
-                "뜻대로 움직여 주지 않는다. 주위를 둘러 보니 여러 명의 사람이 갇혀 있다."+name+"은 방안을 둘러본다.");
+                "뜻대로 움직여 주지 않는다. 주위를 둘러 보니 여러 명의 사람이 갇혀 있다. "+name+"은 방안을 둘러본다.");
 
 
 
